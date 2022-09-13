a01086b9999448ef4ff77578eec2b153d4e64ff5
ssa <wogur981208@gmail.com>
2022-09-13 11:29:54 +0900

    Edit 클래쓰를 쉽게 이해하는 법 주석 처리

--- python basic/클래쓰에 대한 이해/클래스를 쉽게 이해하는 법!.py
+++ python basic/클래쓰에 대한 이해/클래스를 쉽게 이해하는 법!.py
@@ -4,11 +4,11 @@ class Jss:
         self.age = input("나이: ")
     def show(self):
         print(f"나의 이름은 {self.name}, 나이는{self.age}입니다")
-class Jss2(Jss):
+class Jss2(Jss): #상속할 때 불러올 class명을 적는다
     def __init__(self):
-        super().__init__()
+        super().__init__() #상속할 supper로 이전 __init__을 불러온다
         self.gender = input("성별: ")
     def show(self):
-        print(f"나의 이름은 {self.name}, 성별은{self.gender}자, 나이는{self.age}입니다")
+        print(f"나의 이름은 {self.name}, 성별은 {self.gender}자, 나이는{self.age}입니다")
 a = Jss2()
 a.show()
