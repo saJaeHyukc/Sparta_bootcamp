41909478b6043fcc145dabc4f15329018a266473
ssa <wogur981208@gmail.com>
2022-09-13 17:00:31 +0900

    Add 클래스과제!

---
+++ python basic/과제/나의 클래스 과제/클래스 과제.py
@@ -0,0 +1,68 @@
+# #도형 넓이 계산기
+# class Area:
+#     def __init__(self):
+#         self.width = int(input("가로를 적으세요:"))
+#         self.vertical = int(input("세로(높이)를 적으세요:"))
+#         self.PIE = 3.14
+
+#     def square(self):
+#         print(self.width * self.vertical)
+
+#     def triangle(self):
+#         print(self.width * self.vertical / 2)
+
+#     def circle(self):
+#         print((self.width/2)* (self.width/2) * self.PIE)
+
+# area = Area()
+# area.square()
+# area.triangle()
+# area.circle()
+
+# 계산기 만들어보기
+# class Calc:
+#     def set_number(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+        
+#     def plus(self):
+#         print(self.num1 + self.num2)
+        
+#     def minus(self):
+#         print(self.num1 - self.num2)
+        
+#     def mutiple(self):
+#         print(self.num1 * self.num2)
+    
+#     def divide(self):
+#         print(self.num1 / self.num2)
+        
+# calc = Calc()
+# calc.set_number(20, 10)
+# calc.plus()
+# calc.minus()
+# calc.mutiple()
+# calc.divide()
+
+# 프로필 관리 기능 만들어보기
+# class Profile:
+#     def __init__(self):
+#         self.name = input("이름을 적어주세요!:")
+#         self.gender = input("성별을 적어주세요!:")
+#         self.birthday = input("생일을 적어주세요!:")
+#         self.age = input("나이를 적어주세요!:")
+#         self.phone = input("휴대폰 번호를 적어주세요!:")
+#         self.email = input("Email를 적어주세요!:")
+        
+#     def profile_info(self):
+#         print(f'나의 이름은{self.name}')
+#         print(f'나의 성별은{self.gender}')
+#         print(f'나의 생일은{self.birthday}')
+#         print(f'나의 나이는{self.age}')
+#         print(f'나의 휴대폰은{self.phone}')
+#         print(f'나의 이메일은{self.email}')
+        
+
+# profile = Profile()
+# profile.profile_info()
+
