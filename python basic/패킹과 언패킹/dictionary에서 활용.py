c492edaddb357cb250a8eb5f5964b4941a4db32f
ssa <wogur981208@gmail.com>
2022-09-15 13:51:44 +0900

    Add 패킹과 언패킹

---
+++ python basic/패킹과 언패킹/dictionary에서 활용.py
@@ -0,0 +1,42 @@
+def set_profile(**kwargs):
+    profile = {}
+    profile["name"] = kwargs.get("name", "-")
+    profile["gender"] = kwargs.get("gender", "-")
+    profile["birthday"] = kwargs.get("birthday", "-")
+    profile["age"] = kwargs.get("age", "-")
+    profile["phone"] = kwargs.get("phone", "-")
+    profile["email"] = kwargs.get("email", "-")
+    
+    return profile
+
+user_profile = {
+    "name": "lee",
+    "gender": "man",
+    "age": 32,
+    "birthday": "01/01",
+    "email": "python@sparta.com",
+}
+
+print(set_profile(**user_profile))
+""" 아래 코드와 동일
+profile = set_profile(
+    name="lee",
+    gender="man",
+    age=32,
+    birthday="01/01",
+    email="python@sparta.com",
+)
+"""
+
+# result print
+"""
+{
+    'name': 'lee',
+    'gender': 'man',
+    'birthday': '01/01',
+    'age': 32,
+    'phone': '-',
+    'email': 'python@sparta.com'
+}
+
+"""
