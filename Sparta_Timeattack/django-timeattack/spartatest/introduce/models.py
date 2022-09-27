d249b228e359e807a1994a2efe78716753452590
ssa <wogur981208@gmail.com>
2022-09-27 09:51:00 +0900

    Add 쪽지시험

--- Sparta_Timeattack/django-timeattack/spartatest/introduce/models.py
+++ Sparta_Timeattack/django-timeattack/spartatest/introduce/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return f'{self.location}에 {self.created_at}에 접속'
