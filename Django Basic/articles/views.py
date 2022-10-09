a701af68d2889d2f57dd542e892e7d650882f87b
ssa <wogur981208@gmail.com>
2022-10-09 21:59:25 +0900

    Feat: 변수 url
    - 1. articles/views.py, templates/dinner.html, django_tutorial/urls.py
    - 2. string으로 값을 전달하여 views.py에서 전달한 값으로 dinner.html에
      보여줌

--- Django Basic/articles/views.py
+++ Django Basic/articles/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def dinner(request):
+def dinner(request, name):
     menus = ['족발', '햄버거', '치킨', '초밥']
     pick = random.choice(menus)
     context ={
         'pick':pick,
+        'name':name
     }
     return render(request, 'dinner.html', context)
