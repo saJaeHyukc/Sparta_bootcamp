00027fe3681bb7fd80326fd04768c01d4889d51c
ssa <wogur981208@gmail.com>
2022-10-09 23:39:27 +0900

    Feat: model 작성
    - 1. 수정 articles/models.py, articles/admin.py
    - 2. Article 테이블을 만들고 어드민 페이지에 적용시켜주기 위해서

--- Django Basic/django_tutorial/urls.py
+++ Django Basic/django_tutorial/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
-    path('foods/'. include('foods.urls')),
+    path('foods/', include('foods.urls')),
 ]
