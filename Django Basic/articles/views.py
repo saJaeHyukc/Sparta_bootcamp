2f4fa879dbe8080158e3dee777aa24f7215a0c19
ssa <wogur981208@gmail.com>
2022-10-09 23:55:37 +0900

    Feat: 리뷰 리스트
    - 1. 수정   articles/views.py, dinner.html
    - 2. dinner함수에 Article 테이블에 있는 데이터를 불러와 dinner.html에
      보여줌

--- Django Basic/articles/views.py
+++ Django Basic/articles/views.py
@@ -1,17 +1,21 @@
 import random 
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def dinner(request, name):
-    menus = [{"name":'족발', "price": 30000}, {"name": '햄버거', "price": 5000}, {"name": '치킨', "price": 20000}, {"name":'초밥', "price": 15000}]
+    menus = [{"name":'족발', "price": 30000}, {"name": '햄버거', "price": 5000}, 
+            {"name": '치킨', "price": 20000}, {"name":'초밥', "price": 15000}]
     pick = random.choice(menus)
+    articles = Article.objects.all()
     context ={
         'pick':pick,
         'name':name,
         'menus':menus,
+        'articles':articles,
     }
     return render(request, 'dinner.html', context)
 
