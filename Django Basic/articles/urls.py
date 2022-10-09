d51d46a372edcc814a4a01f6e8b63c605b11967d
ssa <wogur981208@gmail.com>
2022-10-09 22:58:59 +0900

    Feat: url namespace
    - 1. 수정 articles/urls.py, articles/review.html
    - 2. url에 각각의 name을 붙여 확장성에 용이하게 만들기 위해

--- Django Basic/articles/urls.py
+++ Django Basic/articles/urls.py
@@ -3,7 +3,7 @@ from articles import views
 urlpatterns = [
     path('index/', views.index),
     path('dinner/<str:name>/', views.dinner),
-    path('review/', views.review),
-    path('create_review/', views.create_review), 
+    path('review/', views.review, name='review'),
+    path('create_review/', views.create_review, name='create_review'), 
 ]
 
